310c7fa11b8dc656f61cffcde561f795aa37901b
Jack_McKing <mactavishking@gmail.com>
2018-10-24 18:39:41 +0800

    1. add read csv header method

--- CSV/CSV.py
+++ CSV/CSV.py
@@ -1,13 +1,16 @@
 import csv
 
-class CSVHandler:
 
+class CSVHandler:
 
     def __init__(self, file_path):
         self.file_path = file_path
 
     def read_table_head(self):
-        with open(self.file_path, 'r') as csv_file:
-            reader = csv.reader(csv_file)
-            for line in reader:
-                print(line)
+        with open(self.file_path, 'rb') as csv_file:
+            header = next(csv_file)
+            header = header.decode()
+            header = header.replace('\r', '')
+            header = header.replace('\n', '')
+            header_list = header.split(",")
+            return header_list
--- demo.py
+++ demo.py
@@ -2,4 +2,5 @@ from CSV.CSV import CSVHandler
 
 if __name__ == '__main__':
     ch = CSVHandler('./resource/testfile.csv')
-    ch.read_table_head()
+    ret = ch.read_table_head()
+    print(ret)
